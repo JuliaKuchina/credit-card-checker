c795b1fba32794efd265c6fc821dd696f0c8a15b
Julia <kuchina@gmail.com>
2019-04-23 01:49:02 +1000

    prod build

---
+++ build/precache-manifest.a4b1913067a358d8ab35538bafbd74ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2f029a4670f78ec3c9d",
+    "url": "/static/js/main.a10bcf4d.chunk.js"
+  },
+  {
+    "revision": "0064a01dfdaff88e72a2",
+    "url": "/static/js/2.829b86ec.chunk.js"
+  },
+  {
+    "revision": "e259b888adfd1383cc5fc8dad7b7999f",
+    "url": "/index.html"
+  }
+];
