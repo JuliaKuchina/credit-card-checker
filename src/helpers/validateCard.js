db1c79342f250a673e869910bcc105ce4b3b145c
Julia <kuchina@gmail.com>
2019-04-23 02:15:22 +1000

    credit-card-checker bug fix

--- src/helpers/validateCard.js
+++ src/helpers/validateCard.js
@@ -4,7 +4,7 @@ import { luhnCheck } from './luhnCheck';
 export function validateCard(numberStr, cardType, checkLength) {
   if (!validateFirstNumbers(numberStr)) return false;
   const lengthValid = validateNumberLength(numberStr, cardType);
-  if (checkLength) return lengthValid;
+  if (checkLength && !lengthValid) return false;
   if (lengthValid) return luhnCheck(numberStr);
   return true;
 
