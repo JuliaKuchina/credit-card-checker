0dbcf652057effd70959467523ccbbb714ecdfc9
Julia <kuchina@gmail.com>
2019-04-23 02:08:40 +1000

    Updates

---
+++ precache-manifest.437ccec008241c7d6009e8a281e86fe5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "46121c5b7c1cdcc69efb",
+    "url": "/credit-card-checker/static/js/runtime~main.483c4c65.js"
+  },
+  {
+    "revision": "29cf5548a24fb2d350fe",
+    "url": "/credit-card-checker/static/js/main.cd79fdc9.chunk.js"
+  },
+  {
+    "revision": "74f4e24861e8ed63173d",
+    "url": "/credit-card-checker/static/js/2.988685da.chunk.js"
+  },
+  {
+    "revision": "be0801e50e426dbe1b59ad634e64acdd",
+    "url": "/credit-card-checker/index.html"
+  }
+];
